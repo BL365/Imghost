156ddcb0ad126ea25cbb11344d7fcffb8c0c97e4
mikhail pogodin <bl365@bk.ru>
2020-07-04 01:17:34 +0300

    mini-mini-refactoring

--- src/Auth.js
+++ src/Auth.js
@@ -11,17 +11,16 @@ class Auth extends React.Component {
 
     handleChange(event) {
         this.setState({[event.target.name]: event.target.value});
-    }
-
+    };
 
     handleSubmit(event) {
-        let auth_str = '{"username":"' + this.state.login + '","password":"' + this.state.password + '"}'
+        let auth_str = '{"username":"' + this.state.login + '","password":"' + this.state.password + '"}';
         event.preventDefault();
         let xhr = new XMLHttpRequest();
         xhr.open('POST', 'http://37.195.44.14:80/api/v1/users/login', true);
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.send(auth_str);
-    }
+    };
 
     render() {
         return (
@@ -30,10 +29,10 @@ class Auth extends React.Component {
                 value = {this.state.login} onChange={this.handleChange} /><br />
                 <input type="password" name="password"
                 value = {this.state.password} onChange={this.handleChange} /><br />
-                <input type="submit" />
+                <input type="submit" value="Войти" />
             </form>
         );
     };
-}
+};
 
 export default Auth;
