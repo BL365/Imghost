cc60de429a96f7ac77aff2e80ff6c69fda9519b6
mikhail pogodin <bl365@bk.ru>
2020-07-25 16:48:44 +0300

    checking the completeness of the form. processing server responses

--- src/Auth.js
+++ src/Auth.js
@@ -15,13 +15,17 @@ class Auth extends React.Component {
     };
 
     handleSubmit(event) {
+        //Проверка заполненности полей
+        if (isEmpty(this.state.login) || isEmpty(this.state.password)) {
+            return false;
+        };
+
         //Отправка запроса авторизации
         event.preventDefault();
         let auth_str = '{"username":"' + this.state.login + '","password":"' + this.state.password + '"}';
         let xhrAuth = new XMLHttpRequest();
         xhrAuth.open('POST', 'http://37.195.44.14:7878/api/v1/users/login', true);
-        xhrAuth.setRequestHeader("Content-Type", "application/json");
-        xhrAuth.setRequestHeader("Accept", "application/json, */*;q=0.5");
+        xhrAuth.setRequestHeader("Content-Type", "application/json", "Accept");
         xhrAuth.responseType = 'json';
         xhrAuth.send(auth_str);
 
@@ -29,20 +33,27 @@ class Auth extends React.Component {
         let responseObj;
         xhrAuth.onload = function() {
             responseObj = xhrAuth.response;
-        };
-        if (!isEmpty(responseObj.error)) {
-            alert('Ошибка авторизации: '+responseObj.error);            
-        } else {
-            alert('Успешно авторизован пользователь: '+responseObj.username+', mail: ' +responseObj.email); 
+            if (xhrAuth.status===200) {
+                if (responseObj.hasOwnProperty('token')) {
+                    //успешная авторизация
+                    alert('Успешно авторизован пользователь: '+responseObj.username+', mail: ' +responseObj.email);
+                } else {
+                    //ошибка
+                    console.log('ошибка учетных данных');
+                }
+            } else {
+              //другая ошибка
+              console.log('другая ошибка: ', responseObj);
+            };
         };
     };
 
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <input type="text" name="login"
+                <input type="text" name="login" placeholder="Login"
                 value = {this.state.login} onChange={this.handleChange} /><br />
-                <input type="password" name="password"
+                <input type="password" name="password" placeholder="Password"
                 value = {this.state.password} onChange={this.handleChange} /><br />
                 <input type="submit" value="Войти" />
             </form>
--- src/Registration.js
+++ src/Registration.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Registration.css';
+import { isEmpty } from 'underscore';
 
 class Registration extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {login: '', password: '', email: ''};
+        this.state = {login: '', password1: '', password2: '', email: ''};
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     };
@@ -14,23 +15,48 @@ class Registration extends React.Component {
     }
 
     handleSubmit(event) {
-        let registration_str = '{"username":"' + this.state.login + '","password":"' + this.state.password + '","email":' + this.state.email + '"}';
+        
+        //Проверка совпадения паролей
+            if (!isEmpty(this.state.password1) === true && !isEmpty(this.state.password2) 
+        && this.state.password1 !== this.state.password2) {
+            alert('пароли не совпадают');
+            return false;
+        };
+
+        //Отправка запроса на регистрацию
+        let registration_str = '{"username":"' + this.state.login + '","password":"' + this.state.password2 + '","email":' + this.state.email + '"}';
         event.preventDefault();
-        let xhr = new XMLHttpRequest();
-        xhr.open('POST', 'http://37.195.44.14:80/api/v1/users/', true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(registration_str);
+        let xhrReg = new XMLHttpRequest();
+        xhrReg.open('POST', 'http://37.195.44.14:7878/api/v1/users', true);
+        xhrReg.setRequestHeader("Content-Type", "application/json");
+        xhrReg.setRequestHeader("Accept", "application/json, */*;q=0.5");
+        xhrReg.responseType = 'json';
+        xhrReg.send(registration_str);
+
+        //Обработка ответа сервера
+        let responseObj;
+        xhrReg.onload = function() {
+            responseObj = xhrReg.response;
+        };
+        alert(responseObj);
+        // if (!isEmpty(responseObj.error)) {
+        //     alert('Ошибка регистрации: ');//+responseObj.error            
+        // } else {
+        //     alert('Успешно зарегистрирован пользователь: '); //+responseObj.username+', mail: ' +responseObj.email
+        // };
     };
 
     render() {
         return (
         <div className="Registration">
             <form onSubmit={this.handleSubmit}>
-                <input type="text" name="email"
+                <input type="text" name="email" placeholder="E-mail"
                 value = {this.state.email} onChange={this.handleChange} /><br />
-                <input type="text" name="login"
+                <input type="text" name="login" placeholder="Login"
                 value = {this.state.login} onChange={this.handleChange} /><br />
-                <input type="password" name="password"
+                <input type="password" name="password1" placeholder="Password"
+                value = {this.state.password} onChange={this.handleChange} /><br />
+                <input type="password" name="password2" placeholder="Re-enter password"
                 value = {this.state.password} onChange={this.handleChange} /><br />
                 <input type="submit" value="Зарегистрироваться" />
             </form>
