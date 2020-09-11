dd3ff1addd96875efab79a937e0f6f23dbeb43b7
mikhail pogodin <bl365@bk.ru>
2020-09-11 22:37:13 +0300

    restyling Auth page

--- src/Auth.js
+++ src/Auth.js
@@ -17,9 +17,31 @@ class Auth extends React.Component {
     handleSubmit(event) {
         event.preventDefault();
         //Проверка заполненности полей
-        if (isEmpty(this.state.login) || isEmpty(this.state.password)) {
+        if (isEmpty(this.state.login)) { //если не все поля заполнены
+            document.getElementById("login").classList.add("auth-input-incorrect");
+            document.getElementById("err").style.display = "block";
+            return false;
+        } else {
+            document.getElementById("login").classList.remove("auth-input-incorrect");
+        }
+        ;
+
+        if (isEmpty(this.state.password)) {
+            document.getElementById("password").classList.add("auth-input-incorrect");
             document.getElementById("err").style.display = "block"
             return false;
+        } else { //если все поля заполнены
+            document.getElementById("password").classList.remove("auth-input-incorrect");
+            document.getElementById("err").style.display = "none"
+        };
+        //Проверка длины пароля не меньше 8-ми символов
+        if (this.state.password.length < 8) {
+            document.getElementById("password").classList.add("auth-input-incorrect");
+            document.getElementById("err_length_pass").style.display = "block"
+            return false;
+        } else {
+            document.getElementById("password").classList.remove("auth-input-incorrect");
+            document.getElementById("err_length_pass").style.display = "none"
         };
 
         //Отправка запроса авторизации
@@ -34,32 +56,47 @@ class Auth extends React.Component {
         //Обработка ответа сервера
         let responseObj;
         xhrAuth.onload = function() {
+            document.getElementById("err_auth").style.display = "none"
             responseObj = xhrAuth.response;
             if (xhrAuth.status===200) {
+                document.getElementById("err").style.display = "none"
                 if (responseObj.hasOwnProperty('token')) {
                     //успешная авторизация
+                    document.getElementById("err_auth").style.display = "none"
                     alert('Успешно авторизован пользователь: '+responseObj.username+', mail: ' +responseObj.email);
                 } else {
                     //ошибка
                     console.log('ошибка учетных данных');
+                    document.getElementById("err_auth").style.display = "block"
                 }
             } else {
               //другая ошибка
               console.log('другая ошибка: ', responseObj);
+              document.getElementById("err").style.display = "block"
             };
         };
     };
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <input type="text" name="login" placeholder="Login"
-                value = {this.state.login} onChange={this.handleChange} /><br />
-                <input type="password" name="password" placeholder="Password"
-                value = {this.state.password} onChange={this.handleChange} /><br />
-                <span id="err">Не все поля заполнены</span>
-                <input type="submit" value="Войти" />
-            </form>
+            <div className="auth-container">
+                <div className="auth-form-invisible">
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="auth-form-visible brd-rad-med">
+                            <input type="text" name="login" placeholder="Login" className="auth-input auth-input-correct brd-rad-medium" maxLength="255" id="login"
+                            value = {this.state.login} onChange={this.handleChange} />
+                            <span id="err_auth" className="auth-form-errors">Неверный логин/пароль</span>
+                            <input type="password" name="password" placeholder="Password" className="auth-input auth-input-correct brd-rad-medium btm-elem" maxLength="255" id="password"
+                            value = {this.state.password} onChange={this.handleChange} />
+                            <span id="err" className="auth-form-errors">Не все поля заполнены</span>
+                            <span id="err_length_pass" className="auth-form-errors">Пароль должен быть не короче 8-ми символов</span>
+                        </div>
+                        <button className="btn brd-rad-min">Join</button>
+                    </form>
+                    <span id="err_connection" className="auth-form-errors">Ошибка связи с сервером, попробуйте позже</span>
+
+                </div>
+            </div>
         );
     };
 };
